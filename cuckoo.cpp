16409dbef4df99706d75518a783a75199c2ebac9
ManuAFK28 <manustdev@gmail.com>
2025-10-20 18:17:47 +0000

    implementacion de la logica del cuckoo hash y redimension de las tablas

---
+++ cuckoo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class CuckooHash {
+private:
+    vector<int> tabla1;
+    vector<int> tabla2;
+    int size;
+    int maxIntentos;
+    int vacio = INT_MIN;
+
+    int hash1(int key) {
+        return key % size;
+    }
+
+    int hash2(int key) {
+        return (key / size) % size;
+    }
+
+    void rehash() {
+        cout << "Rehash activado. Nuevo tamano: " <<size * 2 << endl;
+        vector<int> temporal;
+
+        // guarda los datos actuales
+        for (int x : tabla1) if (x != vacio) temporal.push_back(x);
+        for (int x : tabla2) if (x != vacio) temporal.push_back(x);
+
+        // duplica el size
+        size *= 2;
+        tabla1.assign(size, vacio);
+        tabla2.assign(size, vacio);
+        maxIntentos += 5; 
+    }
+
+int main() {
+
+}
